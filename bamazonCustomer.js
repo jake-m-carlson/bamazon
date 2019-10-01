233203a46587f75280389fb3f7487528a317e4b3
Jake Carlson <jakecarlson@Jakes-MBP.zyxel.com>
2019-10-01 00:19:40 -0500

    intital prompts working

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -31,6 +31,47 @@ connection.connect(function(err) {
       })
   }
 
+  // function to give client prompts
+function customerPrompts(res) {
+    inquirer.prompt([{
+        type: "input", 
+        name: "choice",
+        message: "What would you like to purchase? [Exit with E]"
+    }]).then(function(answer){
+        var correct = false;
+        for(var i = 0; i < res.length; i++){
+            if(res[i].product_name==answer.choice){
+                correct=true;
+                var product=answer.choice;
+                var id=i;
+                inquirer.prompt({
+                    type: "input",
+                    name: "quantity",
+                    message: "How many would you like to buy?",
+                    validate: function(value){
+                        if(isNaN(value)==false){
+                            return true;
+                        } else {
+                            return false;
+                        }
+                    }
+                }).then(function(answer){
+                    if((res[id].stock_quantity-answer.quantity)>0){
+                        connection.query("UPDATE products SET stock_quantity=' " +(res[id].stock_quantity-answer.quantity)+ " ' WHERE product_name' " + product + "'", function(err, res2){
+                            console.log("Product Bought!");
+                            makeTable();
+                        })
+                    } else {
+                        console.log("Not a valid selection!");
+                        customerPrompts(res);
+                        // up until here works
+                    }
+                })
+            }
+        }
+    })
+}
+
 //   function start() {
 //     inquirer
 //       .prompt({
